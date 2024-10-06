0f30145a94657eaebeea588ae530cfbeb63d7c7d
Sujit Mali <93429319+sujit1817@users.noreply.github.com>
2024-10-06 21:06:30 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -80,3 +80,41 @@ public class Main
 		System.out.println("Max element in above ArrayList is : "+max);
 	}
 }
+
+
+//container with most water (brute force)
+import java.util.*;
+
+public class Main
+{
+    public static int storeWater(ArrayList<Integer> height){
+        int maxWater=0;
+        
+        for(int i=0; i<height.size(); i++){
+            for(int j=i+1; j<height.size(); j++){
+            int ht=Math.min(height.get(i),height.get(j));
+            int width=j-1;
+            //area
+            int currWater=ht*width;
+            maxWater = Math.max(maxWater, currWater);
+            }
+        }
+        return maxWater;
+    }
+    
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+		//1,8,6,2,5,4,8,3,7
+		list.add(1);
+		list.add(8);
+		list.add(6);
+		list.add(2);
+		list.add(5);
+		list.add(4);
+		list.add(8);
+		list.add(3);
+		list.add(7);
+		
+		System.out.println("Max water is : "+storeWater(list));
+	}
+}
