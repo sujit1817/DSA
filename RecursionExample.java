f19ed8b441cb0f654d117c77a85836644e2ef99b
Sujit Mali <93429319+sujit1817@users.noreply.github.com>
2025-12-26 22:22:54 +0530

    Create RecursionExample.java

---
+++ RecursionExample.java
@@ -0,0 +1,27 @@
+Recursion :
+Function Calling itself
+
+Important steps:=>
+1. base case
+2. work
+3. inner function call
+
+f(n) = n * f(n-1)
+
+//Qn. Print Desc  
+public class Recursion1 {
+
+    public static void printDesc(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDesc(n-1);     
+    }
+
+    public static void main(String[] args) {
+        int num = 10;
+        printDesc(num);
+    }
+}
