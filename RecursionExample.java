3b5671cc43fa5cdbd32763cb59a9943b4c6cff9d
Sujit Mali <93429319+sujit1817@users.noreply.github.com>
2025-12-26 22:28:21 +0530

    Update RecursionExample.java

--- RecursionExample.java
+++ RecursionExample.java
@@ -25,3 +25,21 @@ public class Recursion1 {
         printDesc(num);
     }
 }
+
+
+//Qn printAsc
+public class Recursion2 {
+    public static void printAsc(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        printAsc(n-1);
+        System.out.print(" "+n);
+    }
+    public static void main(String[] args) {
+        int num = 10;
+        printAsc(num);
+    }
+}
+
